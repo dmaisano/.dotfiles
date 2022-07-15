383e2adf55543de213a122c0a4d7bb5479360de6
Domenico Maisano <dmaisanooo@gmail.com>
2022-07-15 13:16:27 -0400

    feat: added gbr command

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,16 @@ if status is-interactive
     starship init fish | source
 end
 
-set -gx NVM_DIR ~/.nvm
-function nvm
-    bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
-end
+# set -gx NVM_DIR ~/.nvm
+# function nvm
+#     bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+# end
 
 fish_add_path /usr/local/sbin
+
+# alias python="/usr/local/bin/python3"
+
+# Delete all local branches except master or current working branch
+function gbr
+    git branch | grep -Ev "master|$(git branch --show-current)" | xargs git branch -D
+end
