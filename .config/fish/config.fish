136651423b6f181e3e988e477e81d0ac9d0c131a
Dom Maisano <dmaisanooo@gmail.com>
2023-01-08 16:31:19 -0500

    added $IS_WSL and $IS_MAC variables

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,6 +5,20 @@ if status is-interactive
     starship init fish | source
 end
 
+set -gx IS_WSL 0
+if test (grep -i Microsoft /proc/version)
+    set -gx IS_WSL 1
+end
+
+set -gx IS_MAC 0
+
+if test $IS_WSL -eq 1
+    # set DISPLAY variable to the IP automatically assigned to WSL2
+    set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
+    # Automatically start dbus
+    sudo /etc/init.d/dbus start &> /dev/null
+end
+
 # set -gx NVM_DIR ~/.nvm
 # function nvm
 #     bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
@@ -14,17 +28,18 @@ fish_add_path /usr/local/sbin
 
 # alias python="/usr/local/bin/python3"
 
+# run a typescript file with applicable dotenv config
+function ts_script
+    npx ts-node -r dotenv/config $argv
+end
+
 # Delete all local branches except master or current working branch
 function gbr
     git branch | grep -Ev "master|(git branch --show-current)" | xargs git branch -D
 end
 
-# set DISPLAY variable to the IP automatically assigned to WSL2
-set -gx DISPLAY (cat /etc/resolv.conf | grep nameserver | awk '{print $2; exit;}'):0.0
-# Automatically start dbus
-sudo /etc/init.d/dbus start &> /dev/null
-
 # pnpm
+# set -gx PNPM_HOME "/home/virtualdom/.local/share/pnpm"
 set -gx PNPM_HOME "$HOME/.local/share/pnpm"
 set -gx PATH "$PNPM_HOME" $PATH
 # pnpm end
