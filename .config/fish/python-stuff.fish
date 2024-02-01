319cc0eb61fd748bf5aa16bed75194b969cb0a86
dmaisano <dmaisanooo@gmail.com>
2024-02-01 16:23:32 -0500

    Notes for arch linux

--- .config/fish/python-stuff.fish
+++ .config/fish/python-stuff.fish
@@ -2,11 +2,8 @@
 set -gx PYENV_ROOT "$HOME/.pyenv"
 fish_add_path "$PYENV_ROOT/bin"
 pyenv init - | source
-# fish_add_path "$PYENV_ROOT/shims"
-# echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init --path)"\nfi' >> ~/.bashrc
 
-# pyenv init - https://gist.github.com/entropiae/326611addf6662d1d8fbf5792ab9a770#file-install-pyenv-on-ubuntu-18-04-fish-shell-L19
-# set -U fish_user_paths $HOME/.pyenv/bin $fish_user_paths
-# if command -v pyenv 1>/dev/null 2>&1
-#     pyenv init - | source
-# end
+# ? resource - https://www.youtube.com/watch?v=31WU0Dhw4sk
+
+### ? pyenv-virtualenv plugin install
+# git clone https://github.com/pyenv/pyenv-virtualenv.git $(pyenv root)/plugins/pyenv-virtualenv
