3939a4595d915784f28bd0bdd043f7d5d15748cf
dmaisano <dmaisanooo@gmail.com>
2024-08-01 23:16:21 -0400

    chore: remove commented out pyenv configuration

--- .config/fish/python.fish
+++ .config/fish/python.fish
@@ -4,6 +4,6 @@
 # make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
 
 # pyenv https://github.com/pyenv/pyenv?tab=readme-ov-file#set-up-your-shell-environment-for-pyenv
-set -gx PYENV_ROOT "$HOME/.pyenv"
-fish_add_path "$PYENV_ROOT/bin"
-pyenv init - | source
+# set -gx PYENV_ROOT "$HOME/.pyenv"
+# fish_add_path "$PYENV_ROOT/bin"
+# pyenv init - | source
