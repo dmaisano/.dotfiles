695f561f7c0bdcd613e28230332c3acd12b17108
dmaisano <dmaisanooo@gmail.com>
2024-08-01 23:16:06 -0400

    chore: added poetry autocomplete; cleanup config.fish

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,29 +1,15 @@
 if status is-interactive
     ### Commands to run in interactive sessions can go here
     scheme set tokyonight
-
     starship init fish | source
 end
 
 source "$HOME/.config/fish/os.fish"
 source "$HOME/.config/fish/macros.fish"
-source "$HOME/.config/fish/python.fish"
-
-# rust
-# bass source "$HOME/.cargo/env"
+source "$HOME/.config/fish/nodejs.fish"
+# source "$HOME/.config/fish/python.fish"
 
 fish_add_path /usr/local/sbin
-
-# pnpm
-set -gx PNPM_HOME "$HOME/.local/share/pnpm"
-if not string match -q -- $PNPM_HOME $PATH
-    set -gx PATH "$PNPM_HOME" $PATH
-end
-# pnpm end
-
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
 
 # https://direnv.net/docs/hook.html
 # direnv hook fish | source
