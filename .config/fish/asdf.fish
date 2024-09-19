3673fc3f8819820ad2428fc5406f1120eb6613c7
dmaisano <dmaisanooo@gmail.com>
2024-09-19 01:03:45 -0400

    .

--- .config/fish/asdf.fish
+++ .config/fish/asdf.fish
@@ -1,4 +1,9 @@
 source "$HOME/.asdf/asdf.fish"
+source "$HOME/.asdf/plugins/golang/set-env.fish"
 
-# list of plugins to manually install
+# if type -q asdf
+#     asdf init
+# end
+
+# ? list of plugins to manually install
 # asdf plugin add golang
