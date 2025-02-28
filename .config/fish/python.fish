930784d7a5e0d3b59996cbadc3cc96124d3e28e8
Dom Maisano <dmaisano808@gmail.com>
2025-02-28 12:10:12 -0500

    Delete .config/fish/python.fish

--- .config/fish/python.fish
+++
@@ -1,9 +0,0 @@
-# ? resource - https://www.youtube.com/watch?v=31WU0Dhw4sk
-
-# ? pre-requisite packages
-# make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev wget curl llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev libffi-dev liblzma-dev python3-openssl git
-
-# pyenv https://github.com/pyenv/pyenv?tab=readme-ov-file#set-up-your-shell-environment-for-pyenv
-set -gx PYENV_ROOT "$HOME/.pyenv"
-fish_add_path "$PYENV_ROOT/bin"
-pyenv init - | source
