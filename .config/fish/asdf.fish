0d948fbe10ce7c34aaa2cb613c1a370b5c44188a
Dom Maisano <dmaisano808@gmail.com>
2025-03-04 09:49:30 -0500

    ASDF_GOLANG_MOD_VERSION_ENABLED=true

--- .config/fish/asdf.fish
+++ .config/fish/asdf.fish
@@ -4,5 +4,6 @@ if test -d "$HOME/.asdf"
     if test -d "$HOME/.asdf/plugins/golang"
         source "$HOME/.asdf/plugins/golang/set-env.fish"
         fish_add_path $GOBIN
+        set -gx ASDF_GOLANG_MOD_VERSION_ENABLED true
     end
 end
