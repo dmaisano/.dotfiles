ed7a4af69115b62547cb25395c1706eeaf6aa401
Dom Maisano <dmaisano808@gmail.com>
2025-07-07 10:39:05 -0400

    git macros

--- .config/fish/macros.fish
+++ .config/fish/macros.fish
@@ -1,3 +1,7 @@
+# ? git
+abbr --add --set-cursor gsl "git stash list"
+abbr --add --set-cursor gsm 'git stash -m "%"'
+
 # ? github cli
 abbr --add --set-cursor copilot "gh copilot %"
 abbr --add --set-cursor gcs "gh copilot suggest %"
